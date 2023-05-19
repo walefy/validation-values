25848ed121efa9aa608ec47d7652bbd4f4bb3fbf
Walefy D. Gonçalves <walefyd1@gmail.com>
2023-05-19 20:05:29 -0300

    Construção da aplicação de validação iniciada

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import validator from 'validator';
+import './style.css';
+
+const inputValue = document.querySelector('#value');
+const identificationType = document.querySelector('#identificationType');
+const submitBtn = document.querySelector('button');
+const outputPEl = document.querySelector('p');
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const UUID_VERSION = 4;
+
+  const options = {
+    email: validator.isEmail(inputValue.value),
+    cpf: validator.isTaxID(inputValue.value, 'pt-BR'),
+    hexColor: validator.isHexColor(inputValue.value),
+    uuid: validator.isUUID(inputValue.value, UUID_VERSION),
+    url: validator.isURL(inputValue.value),
+  };
+
+  try {
+    const verification = options[identificationType.value];
+    outputPEl.innerText = `A validação retornou ${verification}`;
+  } catch (error) {
+    outputPEl.innerText = `A validação falhou com o erro: ${error.message}`;
+  }
+});
